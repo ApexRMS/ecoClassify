164929da7b105f23ea8622d3064712a83245f40e
afilazzola <alex.filazzola@outlook.com>
2024-10-31 09:51:50 -0400

    added core check against number of jobs requested

--- imageclassifier/workspace.r
+++ imageclassifier/workspace.r
@@ -81,6 +81,37 @@ assignVariables <- function(myScenario,
               modelType))
 }
 
+## Set cores
+
+#' Set the number of cores for multiprocessing
+#'
+#' @description
+#' 'setCores' determines the number of cores to use for multiprocessing based on the available cores and user settings.
+#'
+#' @param mulitprocessingSheet A dataframe containing multiprocessing settings. It should have the columns 'EnableMultiprocessing' (logical) and 'MaximumJobs' (integer).
+#' @return The number of cores to use for multiprocessing (integer).
+#'
+#' @details
+#' The function first detects the number of available cores on the system. If multiprocessing is enabled in the 'mulitprocessingSheet', it checks if the requested number of cores exceeds the available cores. If so, it sets the number of cores to one less than the available cores and issues a warning. Otherwise, it sets the number of cores to the requested number. If multiprocessing is not enabled, it sets the number of cores to 1.
+#' @noRd
+setCores <- function(mulitprocessingSheet) {
+  availableCores <- parallel::detectCores()
+  if(mulitprocessingSheet$EnableMultiprocessing) {
+    requestedCores <- mulitprocessingSheet$MaximumJobs
+    if(requestedCores > availableCores) {
+      warning(paste0("Requested number of jobs exceeds available cores. Continuing run with ",availableCores," jobs."))
+      nCores <- availableCores - 1
+    } else {
+      nCores <- requestedCores
+    }
+  } else {
+    nCores <- 1
+  }
+  return(nCores)
+}
+
+
+
 #' Extract rasters from filepaths in a dataframe
 #'
 #' @description
